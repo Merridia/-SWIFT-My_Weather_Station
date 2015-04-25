35cadf4cf06af2c612d320fa344f961023550b53
Archloy <Archloy@users.noreply.github.com>
2015-04-25 16:06:36 +0200

    Create : Datum class (return current day)

---
+++ [JD]My_Weather_Station/datum.swift
@@ -0,0 +1,19 @@
+//
+//  datum.swift
+//  [JD]My_Weather_Station
+//
+//  Created by Developer on 25/04/2015.
+//  Copyright (c) 2015 Jennifer D'ROZA. All rights reserved.
+//
+
+import Foundation
+
+public class Datum
+{
+    class func getDay() -> String
+    {
+        let url = "http://www.timeapi.org/utc/now.json?%25d%20%25m%20%25Y"
+        let data : NSDictionary = GetDatas.parseJSON(url)
+        return data["dateString"] as String
+    }
+}
