c5c3dc7f9f4ca7c3d0f485ed6d522f522bb6ccdf
Archloy <Archloy@users.noreply.github.com>
2015-04-25 16:20:10 +0200

    Fix url error - remove http://, adding auto by getDatas

--- [JD]My_Weather_Station/datum.swift
+++ [JD]My_Weather_Station/datum.swift
@@ -12,7 +12,7 @@ public class Datum
 {
     class func getDay() -> String
     {
-        let url = "http://www.timeapi.org/utc/now.json?%25d%20%25m%20%25Y"
+        let url = "www.timeapi.org/utc/now.json?%25d%20%25m%20%25Y"
         let data : NSDictionary = GetDatas.parseJSON(url)
         return data["dateString"] as String
     }
